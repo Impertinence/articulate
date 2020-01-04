e0cf644dfcecad99436c3f998176a8483f5d9678
Pranav Hegde <p4ndemonium@protonmail.com>
2020-01-04 01:51:43 +0000

    face detection works

---
+++ projectSAM/voiceassistant/voiceassistant.js
@@ -0,0 +1,11 @@
+var say = require('say');
+var GSR = require('google-search-results-nodejs');
+
+//Start up message
+function powerOnMessage() {
+	say.speak("what's good, dog?");
+}
+powerOnMessage();
+
+//Listen for init
+
